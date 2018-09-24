083f3ea24e2c50ab992a980fd749361e2e7a6d40
SB <sb@SBs-MacBook-Pro.fios-router.home>
2018-09-23 21:09:10 -0400

    starting a new project Homepwner (no typo); following MVC, implemented a UITableViewController and a custom Item class (NSObject type) for data

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by SB on 9/23/18.
+//  Copyright © 2018 SB. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
