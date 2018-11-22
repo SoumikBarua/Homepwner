b572b6accf755a7de33dd3ad77ae944065317b6d
SB <sb@SBs-MacBook-Pro.fios-router.home>
2018-11-22 11:49:41 -0500

    On a new branch (the original will be needed later on), created two sections for the tableView with the first one listing items with value over 50 and the second one listing the rest, implemented section headers; first of the three challenges

--- Homepwner/AppDelegate.swift
+++ Homepwner/AppDelegate.swift
@@ -10,46 +10,59 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         // Create an ItemStore
         let itemStore = ItemStore()
-
+        var moreThan50 = [Item]()
+        var theRest = [Item]()
+        
+        for item in itemStore.allItems {
+            //print("item.valueInDollars is \(item.valueInDollars)")
+            if item.valueInDollars > 50 {
+                moreThan50.append(item)
+                //print("added to moreThan50")
+            } else {
+                theRest.append(item)
+                //print("added to theRest")
+            }
+        }
         
         // Access the ItemsViewController and set its itemStore
         let itemsController = window!.rootViewController as! ItemsViewController
         itemsController.itemStore = itemStore
+        itemsController.moreThan50 = moreThan50
+        itemsController.theRest = theRest
         
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
-
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -11,23 +11,57 @@ import UIKit
 class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
+    var moreThan50: [Item]!
+    var theRest: [Item]!
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemStore.allItems.count
+        print("section is \(section)")
+        var rowCount: Int = 0
+        
+        if section == 0 {
+            rowCount = moreThan50.count
+        } else if section == 1 {
+            rowCount = theRest.count
+        }
+        return rowCount
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        // Get a new or recycled cell
+        // Creating an instance of UITableViewCell with default appearance
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         
-
-        let item = itemStore.allItems[indexPath.row]
+        if indexPath.section == 0 {
+            let item = moreThan50[indexPath.row]
             
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+
+        }
+        else if indexPath.section == 1 {
+            let item = theRest[indexPath.row]
+        
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        }
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var headerString = String()
+        if section == 0 {
+            headerString = "More than 50"
+        }
+        else if section == 1 {
+            headerString = "The Rest"
+        }
+        return headerString
     }
     
     override func viewDidLoad() {
