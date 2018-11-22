d1bd0c2663d2c99c0624e5241738dd200ad64a18
SB <sb@SBs-MacBook-Pro.fios-router.home>
2018-11-22 11:10:13 -0500

    Implemented a custom ItemStore class (Swift base class), dependency inversion principle for giving the controller access to the store. Also implemented data source methods, reused UITableViewCells with identifier and used content insets

---
+++ Homepwner/ItemStore.swift
@@ -0,0 +1,28 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by SB on 9/24/18.
+//  Copyright © 2018 SB. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+}
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -10,4 +10,34 @@ import UIKit
 
 class ItemsViewController: UITableViewController {
     
+    var itemStore: ItemStore!
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Get a new or recycled cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        
+
+        let item = itemStore.allItems[indexPath.row]
+            
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get the height of the status bar
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        
+        let insets = UIEdgeInsetsMake(statusBarHeight, 0, 0, 0)
+        tableView.contentInset =  insets
+        tableView.scrollIndicatorInsets = insets
+    }
 }
