26f1b86ab28265c77c00857fdf94969fb1b18d2b
SB <sb@SBs-MacBook-Pro.local>
2018-11-23 17:23:33 -0500

    Created an interface for editing the details of an item using UIStackView on a new view controller, labels and textfields of the new view were laid out using nested stack views and content compression resistance priorities, and the new view controller's view was presented using UIStoryboardSegue

---
+++ Homepwner/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by SB on 11/23/18.
+//  Copyright © 2018 SB. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    
+    var item: Item!
+    
+    let numberFormatter: NumberFormatter = {
+       let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.maximumFractionDigits = 2
+        nf.minimumFractionDigits = 2
+        return nf
+    }()
+    
+    let dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .medium
+        df.timeStyle = .none
+        return df
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+        valueField.text = numberFormatter.string(from: NSNumber(value: item.valueInDollars))
+        dateLabel.text = dateFormatter.string(from: item.dateCreated)
+    }
+}
